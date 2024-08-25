68bc4e9e537d9b57f6615f963c7ec816505d5f32
Akarsh <akarshgupta816@gmail.com>
2024-08-25 19:44:40 +0530

    app file set up

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -14,6 +14,19 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 
 
+//import routes
+import {userRoute} from "./routes/user.routes.js"
+import {companyRoute} from "./routes/company.routes.js"
+import {jobRoute} from "./routes/job.routes.js"
+import {applicationRoute} from "./routes/application.routes.js"
+
+
+// set up routes
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
+
 
 
 
